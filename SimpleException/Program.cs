1369a0b8c491b3f71f75ec99796618b6116e5149
Covali Andrei <krab4ek@gmail.com>
2022-07-07 22:32:04 +0300

    add new 2 class

--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console
+﻿using SimpleException;
+Console.WriteLine("****** Simple Exception Example *****\n");
+Console.WriteLine($"=> Creating a car and stepping on it!");
+
+Car myCar = new Car("Zippy",45);
+myCar.CrankTunes(true);
+ for (int i = 0; i < 10; i++)
+    myCar.Accelerate(10);
+
+Console.ReadLine();
